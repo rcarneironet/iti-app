6dd71ee39eb755ab63c8eb842e114691aea449ef
Ray Carneiro <ray@academiadotnet.com.br>
2020-05-13 18:10:46 -0300

    inclusao api e testes unitarios

---
+++ src/Iti.AppDemo.API/Controllers/PasswordController.cs
@@ -0,0 +1,29 @@
+﻿using Iti.AppDemo.Application.Handlers;
+using Iti.AppDemo.Core.Commands;
+using Iti.AppDemo.Domain.Contexts.Commands.PasswordCommands;
+using Iti.AppDemo.Domain.Contexts.Commands.PasswordCommands.Output;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Iti.AppDemo.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PasswordController : Controller
+    {
+        private readonly PasswordHandler _handler;
+        public PasswordController(PasswordHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpPost("ValidatePassword")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public ICommandResult Post([FromBody]CreatePasswordCommand command)
+        {
+            var result = (CreatePasswordCommandResult)_handler.Handle(command);
+            return result;
+        }
+    }
+}
---
+++ src/Iti.AppDemo.API/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Iti.AppDemo.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "ITI",
+                    Description = "API ITI",
+                    Contact = new Contact { Name = "Itau" },
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "ITI API v1.0");
+            });
+        }
+    }
+}
